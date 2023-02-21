dd83b63a008b186dfa3b84921c8af139e965a7c4
usamaTheFirst <usamafiaz1453@gmail.com>
2023-02-21 22:13:19 +0500

    auth

---
+++ SAPConnection/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SAPConnection.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public int Pno { get; set; }
+       
+    }
+}
--- SAPConnection/Program.cs
+++ SAPConnection/Program.cs
@@ -2,15 +2,22 @@ using Blazored.Toast;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SAPConnection.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SAPConnection.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SAPConnectionContextConnection") ?? throw new InvalidOperationException("Connection string 'SAPConnectionContextConnection' not found.");
+
+builder.Services.AddDbContext<SAPConnectionContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SAPConnectionContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddBlazoredToast();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +33,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
