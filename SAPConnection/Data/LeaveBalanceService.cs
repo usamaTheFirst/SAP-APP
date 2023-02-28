28fa899f5c37e61b291bf0bcbe0cbb2ff4c5ac58
Usama Fiaz <usamafiaz1453@gmail.com>
2023-02-28 10:17:29 +0500

    leave bln

---
+++ SAPConnection/Data/LeaveBalanceService.cs
@@ -0,0 +1,24 @@
+﻿using SapNwRfc;
+
+namespace SAPConnection.Data
+{
+    public class LeaveBalanceInputParameter
+    {
+        [SapName("PNO")]
+        public string Pno { get; set; }
+        [SapName("FRODT")]
+        public string FromDate { get; set; }
+        [SapName("TODT")]
+        public string ToDate { get; set; }
+        [SapName("SUBTY")]
+        public string LeaveType { get; set; }
+    }
+
+
+
+    public class LeaveBalanceReturnParameter
+    {
+        [SapName("BAL")]
+        public string Bal { get; set; }
+    }
+}
